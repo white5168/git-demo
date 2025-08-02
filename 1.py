655276dc5741ea79cc3f0e971dbabc93c9158860
amin888 <aminliao888@gmail.com>
2025-08-02 10:25:29 +0800

    改為一次抓取10天新聞

--- 1.py
+++ 1.py
@@ -2,17 +2,27 @@ import requests
 import pandas as pd
 import datetime as dt
 import json
+data = []
 url = "https://api.cnyes.com/media/api/v1/newslist/category/headline" #新聞連結
 payload = {
-    "page":2,
+    "page":1,
     "limit":30,
     "isCategoryHeadline":1,
-    "startAt":int((dt.datetime.today() - dt.timedelta(days = 11)).timestamp()),
+    "startAt":int((dt.datetime.today() - dt.timedelta(days = 10)).timestamp()),
     "endAt":int(dt.datetime.today().timestamp())
 } #參數
 res = requests.get(url, params = payload) #連線鉅亨網
 jd = json.loads(res.text) #解析JSON轉成dict
-df = pd.DataFrame(jd['items']['data']) #取出新聞資料
+data.append(pd.DataFrame(jd['items']['data']))
+
+for i in range(2, jd['items']['last_page'] + 1):
+    print("i = ", i)
+    payload["page"] = i
+    res = requests.get(url, params = payload) #連線鉅亨網
+    jd = json.loads(res.text) #解析JSON轉成dict  
+    data.append(pd.DataFrame(jd['items']['data']))
+    
+df = pd.concat(data, ignore_index = True) #取出新聞資料
 df = df[['newsId', 'title', 'summary']]#取出特定欄位
 df['link'] = df['newsId'].apply(lambda  x: 'https://m.cnyes.com/news/id/' + str(x))#建立連結
 df.to_csv('news.csv', encoding = 'utf-8-sig')
